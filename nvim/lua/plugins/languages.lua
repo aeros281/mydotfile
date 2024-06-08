b0aa89932ad666893ced794f8a9e1c175d4d1496
Tran Le Tuan Anh <tranletuananh@daiwalance.com.vn>
2024-06-08 09:46:19 +0700

    update elixir

--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -4,24 +4,10 @@ return {
         version = "*",
         event = { "BufReadPre *.ex,*.exs", "BufNewFile *.ex,*.exs" },
         config = function()
-            local elixir = require("elixir")
-            local elixirls = require("elixir.elixirls")
-
-            elixir.setup({
-                nextls = { enable = true },
-                credo = {},
-                elixirls = {
-                    enable = true,
-                    settings = elixirls.settings({
-                        dialyzerEnabled = false,
-                        enableTestLenses = false,
-                    }),
-                    on_attach = function(client, bufnr)
-                        vim.keymap.set("n", "<space>fp", ":ElixirFromPipe<cr>", { buffer = true, noremap = true })
-                        vim.keymap.set("n", "<space>tp", ":ElixirToPipe<cr>", { buffer = true, noremap = true })
-                        vim.keymap.set("v", "<space>em", ":ElixirExpandMacro<cr>", { buffer = true, noremap = true })
-                    end,
-                },
+            require("elixir").setup({
+                nextls = { enable = false },
+                credo = { enable = true },
+                elixirls = { enable = true },
             })
         end,
         dependencies = {
