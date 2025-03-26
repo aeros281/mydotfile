ca23b974b178c636054ef1141dd4abde29e9f956
Anh Le Tuan Tran <tranletuananh1992@gmail.com>
2025-03-26 16:42:39 +0700

    update config

--- nvim/lua/anhtran/lsp_config.lua
+++ nvim/lua/anhtran/lsp_config.lua
@@ -54,20 +54,6 @@ if vim.fn.executable('lua-language-server') then
     })
 end
 
-vim.api.nvim_create_augroup("LspAttach_inlayhints", {})
-vim.api.nvim_create_autocmd("LspAttach", {
-    group = "LspAttach_inlayhints",
-    callback = function(args)
-        if not (args.data and args.data.client_id) then
-            return
-        end
-
-        local bufnr = args.buf
-        local client = vim.lsp.get_client_by_id(args.data.client_id)
-        require("lsp-inlayhints").on_attach(client, bufnr)
-    end,
-})
-
 -- Global mappings.
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
--- nvim/lua/plugins/code_completion.lua
+++ nvim/lua/plugins/code_completion.lua
@@ -14,6 +14,7 @@ return {
         config = function()
             require("lsp-inlayhints").setup()
         end,
+        enabled = false,
     },
 
     {
