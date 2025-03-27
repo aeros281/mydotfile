6b651877f6c6fceb6b770fe4d4df35264ac5e045
Tran Le Tuan Anh <tranletuananh1992@gmail.com>
2025-03-27 17:54:20 +0700

    use next ls

--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -4,9 +4,14 @@ return {
         version = "*",
         event = { "BufReadPre *.ex,*.exs", "BufNewFile *.ex,*.exs" },
         config = function()
-            require("elixir").setup({})
+            require("elixir").setup({
+                credo = { enable = true },
+                nextls = { enable = true },
+                elixirls = { enable = false }
+            })
+            })
         end,
-        enabled = true,
+q        enabled = true,
         dependencies = {
             "nvim-lua/plenary.nvim",
         },
