dbb8276ee8246689921719beb79a43af34708ed0
Anh Le Tuan Tran <tranletuananh1992@gmail.com>
2025-03-27 18:31:41 +0700

    update config

--- nvim/lua/plugins/languages.lua
+++ nvim/lua/plugins/languages.lua
@@ -5,9 +5,9 @@ return {
         event = { "BufReadPre *.ex,*.exs", "BufNewFile *.ex,*.exs" },
         config = function()
             require("elixir").setup({
-                credo = { enable = true },
-                nextls = { enable = true },
-                elixirls = { enable = false }
+                credo = { enable = false },
+                nextls = { enable = false },
+                elixirls = { enable = true }
             })
         end,
         enabled = true,
