57ace5402ee8a4d898c126598df34c6d57b2d841
Tran Le Tuan Anh <tranletuananh1992@gmail.com>
2025-06-22 12:08:53 +0700

    null-ls -> none-ls

--- nvim/lua/plugins/code_completion.lua
+++ nvim/lua/plugins/code_completion.lua
@@ -1,218 +1,222 @@
 return {
-    {
-        "saghen/blink.cmp",
-        -- optional: provides snippets for the snippet source
-        dependencies = { "rafamadriz/friendly-snippets", "onsails/lspkind.nvim" },
+	{
+		"saghen/blink.cmp",
+		-- optional: provides snippets for the snippet source
+		dependencies = { "rafamadriz/friendly-snippets", "onsails/lspkind.nvim" },
 
-        -- use a release tag to download pre-built binaries
-        version = "1.*",
-        -- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
-        -- build = 'cargo build --release',
-        -- If you use nix, you can build from source using latest nightly rust with:
-        -- build = 'nix run .#build-plugin',
+		-- use a release tag to download pre-built binaries
+		version = "1.*",
+		-- AND/OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
+		-- build = 'cargo build --release',
+		-- If you use nix, you can build from source using latest nightly rust with:
+		-- build = 'nix run .#build-plugin',
 
-        ---@module 'blink.cmp'
-        ---@type blink.cmp.Config
-        opts = {
-            -- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
-            -- 'super-tab' for mappings similar to vscode (tab to accept)
-            -- 'enter' for enter to accept
-            -- 'none' for no mappings
-            --
-            -- All presets have the following mappings:
-            -- C-space: Open menu or open docs if already open
-            -- C-n/C-p or Up/Down: Select next/previous item
-            -- C-e: Hide menu
-            -- C-k: Toggle signature help (if signature.enabled = true)
-            --
-            -- See :h blink-cmp-config-keymap for defining your own keymap
-            keymap = {
-                preset = "none",
+		---@module 'blink.cmp'
+		---@type blink.cmp.Config
+		opts = {
+			-- 'default' (recommended) for mappings similar to built-in completions (C-y to accept)
+			-- 'super-tab' for mappings similar to vscode (tab to accept)
+			-- 'enter' for enter to accept
+			-- 'none' for no mappings
+			--
+			-- All presets have the following mappings:
+			-- C-space: Open menu or open docs if already open
+			-- C-n/C-p or Up/Down: Select next/previous item
+			-- C-e: Hide menu
+			-- C-k: Toggle signature help (if signature.enabled = true)
+			--
+			-- See :h blink-cmp-config-keymap for defining your own keymap
+			keymap = {
+				preset = "none",
 
-                ["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
-                ["<C-e>"] = { "hide" },
-                ["<C-y>"] = { "select_and_accept" },
-                ["<cr>"] = { "select_and_accept", "fallback_to_mappings" },
+				["<C-space>"] = { "show", "show_documentation", "hide_documentation" },
+				["<C-e>"] = { "hide" },
+				["<C-y>"] = { "select_and_accept" },
+				["<cr>"] = { "select_and_accept", "fallback_to_mappings" },
 
-                ["<Up>"] = { "select_prev", "fallback" },
-                ["<Down>"] = { "select_next", "fallback" },
-                ["<Tab>"] = { "select_next", "fallback" },
-                ["<S-Tab>"] = { "select_prev", "fallback" },
-                ["<C-b>"] = { "scroll_documentation_up", "fallback" },
-                ["<C-f>"] = { "scroll_documentation_down", "fallback" },
+				["<Up>"] = { "select_prev", "fallback" },
+				["<Down>"] = { "select_next", "fallback" },
+				["<Tab>"] = { "select_next", "fallback" },
+				["<S-Tab>"] = { "select_prev", "fallback" },
+				["<C-b>"] = { "scroll_documentation_up", "fallback" },
+				["<C-f>"] = { "scroll_documentation_down", "fallback" },
 
-                ["<c-h>"] = { "snippet_forward", "fallback" },
-                ["<c-n>"] = { "snippet_backward", "fallback" },
+				["<c-h>"] = { "snippet_forward", "fallback" },
+				["<c-n>"] = { "snippet_backward", "fallback" },
 
-                ["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
-            },
+				["<C-k>"] = { "show_signature", "hide_signature", "fallback" },
+			},
 
-            appearance = {
-                -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
-                -- Adjusts spacing to ensure icons are aligned
-                nerd_font_variant = "mono",
-            },
+			appearance = {
+				-- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
+				-- Adjusts spacing to ensure icons are aligned
+				nerd_font_variant = "mono",
+			},
 
-            -- (Default) Only show the documentation popup when manually triggered
+			-- (Default) Only show the documentation popup when manually triggered
 
-            -- Default list of enabled providers defined so that you can extend it
-            -- elsewhere in your config, without redefining it, due to `opts_extend`
-            sources = {
-                default = { "lsp", "path", "snippets", "buffer" },
-            },
+			-- Default list of enabled providers defined so that you can extend it
+			-- elsewhere in your config, without redefining it, due to `opts_extend`
+			sources = {
+				default = { "lsp", "path", "snippets", "buffer" },
+			},
 
-            -- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
-            -- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
-            -- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
-            --
-            -- See the fuzzy documentation for more information
-            fuzzy = { implementation = "prefer_rust_with_warning" },
-            signature = { window = { border = 'single' } },
-            completion = {
-                documentation = { window = { border = 'single' } },
-                menu = {
-                    border = 'single',
-                    draw = {
-                        components = {
-                            kind_icon = {
-                                text = function(ctx)
-                                    local lspkind = require("lspkind")
-                                    local icon = ctx.kind_icon
-                                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                        local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
-                                        if dev_icon then
-                                            icon = dev_icon
-                                        end
-                                    else
-                                        icon = lspkind.symbolic(ctx.kind, {
-                                            mode = "symbol",
-                                        })
-                                    end
+			-- (Default) Rust fuzzy matcher for typo resistance and significantly better performance
+			-- You may use a lua implementation instead by using `implementation = "lua"` or fallback to the lua implementation,
+			-- when the Rust fuzzy matcher is not available, by using `implementation = "prefer_rust"`
+			--
+			-- See the fuzzy documentation for more information
+			fuzzy = { implementation = "prefer_rust_with_warning" },
+			signature = { window = { border = "single" } },
+			completion = {
+				documentation = { window = { border = "single" } },
+				menu = {
+					border = "single",
+					draw = {
+						components = {
+							kind_icon = {
+								text = function(ctx)
+									local lspkind = require("lspkind")
+									local icon = ctx.kind_icon
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, _ = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											icon = dev_icon
+										end
+									else
+										icon = lspkind.symbolic(ctx.kind, {
+											mode = "symbol",
+										})
+									end
 
-                                    return icon .. ctx.icon_gap
-                                end,
+									return icon .. ctx.icon_gap
+								end,
 
-                                -- Optionally, use the highlight groups from nvim-web-devicons
-                                -- You can also add the same function for `kind.highlight` if you want to
-                                -- keep the highlight groups in sync with the icons.
-                                highlight = function(ctx)
-                                    local hl = ctx.kind_hl
-                                    if vim.tbl_contains({ "Path" }, ctx.source_name) then
-                                        local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
-                                        if dev_icon then
-                                            hl = dev_hl
-                                        end
-                                    end
-                                    return hl
-                                end,
-                            },
-                        },
-                    },
-                },
-            },
-        },
-        opts_extend = { "sources.default" },
-    },
-    {
-        "neovim/nvim-lspconfig",
-        dependencies = { "saghen/blink.cmp" },
-        opts = {
-            servers = {
-                lua_ls = {},
-                ts_ls = {},
-                dartls = {},
-                bashls = {},
-                pyright = {},
-                rust_analyzer = {},
-            },
-        },
-        config = function(_, opts)
-            local lspconfig = require("lspconfig")
-            for server, config in pairs(opts.servers) do
-                -- passing config.capabilities to blink.cmp merges with the capabilities in your
-                -- `opts[server].capabilities, if you've defined it
-                config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
-                lspconfig[server].setup(config)
-            end
+								-- Optionally, use the highlight groups from nvim-web-devicons
+								-- You can also add the same function for `kind.highlight` if you want to
+								-- keep the highlight groups in sync with the icons.
+								highlight = function(ctx)
+									local hl = ctx.kind_hl
+									if vim.tbl_contains({ "Path" }, ctx.source_name) then
+										local dev_icon, dev_hl = require("nvim-web-devicons").get_icon(ctx.label)
+										if dev_icon then
+											hl = dev_hl
+										end
+									end
+									return hl
+								end,
+							},
+						},
+					},
+				},
+			},
+		},
+		opts_extend = { "sources.default" },
+	},
+	{
+		"neovim/nvim-lspconfig",
+		dependencies = { "saghen/blink.cmp" },
+		opts = {
+			servers = {
+				lua_ls = {},
+				ts_ls = {},
+				dartls = {},
+				bashls = {},
+				pyright = {},
+				rust_analyzer = {},
+			},
+		},
+		config = function(_, opts)
+			local lspconfig = require("lspconfig")
+			for server, config in pairs(opts.servers) do
+				-- passing config.capabilities to blink.cmp merges with the capabilities in your
+				-- `opts[server].capabilities, if you've defined it
+				config.capabilities = require("blink.cmp").get_lsp_capabilities(config.capabilities)
+				lspconfig[server].setup(config)
+			end
 
-            -- Global mappings.
-            -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-            -- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
-            vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
-            vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
+			-- Global mappings.
+			-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+			-- vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+			vim.keymap.set("n", "[g", vim.diagnostic.goto_prev)
+			vim.keymap.set("n", "]g", vim.diagnostic.goto_next)
 
-            -- Use LspAttach autocommand to only map the following keys
-            -- after the language server attaches to the current buffer
-            vim.api.nvim_create_autocmd("LspAttach", {
-                group = vim.api.nvim_create_augroup("UserLspConfig", {}),
-                callback = function(ev)
-                    -- Enable completion triggered by <c-x><c-o>
-                    vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
+			-- Use LspAttach autocommand to only map the following keys
+			-- after the language server attaches to the current buffer
+			vim.api.nvim_create_autocmd("LspAttach", {
+				group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+				callback = function(ev)
+					-- Enable completion triggered by <c-x><c-o>
+					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 
-                    -- Buffer local mappings.
-                    -- See `:help vim.lsp.*` for documentation on any of the below functions
-                    local local_opts = { buffer = ev.buf }
-                    vim.keymap.set("n", "gD", vim.lsp.buf.declaration, local_opts)
-                    -- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-                    vim.keymap.set("n", "K", vim.lsp.buf.hover, local_opts)
-                    -- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
-                    vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, local_opts)
-                    vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, local_opts)
-                    vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, local_opts)
-                    vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, local_opts)
-                    -- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-                end,
-            })
+					-- Buffer local mappings.
+					-- See `:help vim.lsp.*` for documentation on any of the below functions
+					local local_opts = { buffer = ev.buf }
+					vim.keymap.set("n", "gD", vim.lsp.buf.declaration, local_opts)
+					-- vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
+					vim.keymap.set("n", "K", vim.lsp.buf.hover, local_opts)
+					-- vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, opts)
+					vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, local_opts)
+					vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, local_opts)
+					vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, local_opts)
+					vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, local_opts)
+					-- vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
+				end,
+			})
 
-            require("nvim-treesitter.configs").setup({
-                ensure_installed = {
-                    "elixir",
-                    "heex",
-                    "eex",
-                    "html",
-                    "lua",
-                    "sql",
-                    "html",
-                    "python",
-                    "markdown",
-                    "yaml",
-                }, -- only install parsers for elixir and heex
-                -- ensure_installed = "all", -- install parsers for all supported languages
-                sync_install = false,
-                ignore_install = {},
-                highlight = {
-                    enable = true,
-                    disable = {},
-                },
-            })
-        end,
-    },
-    {
-        "lvimuser/lsp-inlayhints.nvim",
-        config = function()
-            require("lsp-inlayhints").setup()
-        end,
-        enabled = false,
-    },
-    {
-        "nvim-treesitter/nvim-treesitter",
-    },
-    {
-        "nvim-treesitter/nvim-treesitter-textobjects",
-        dependencies = "nvim-treesitter/nvim-treesitter",
-    },
-    {
-        "cuducos/yaml.nvim",
-        ft = { "yaml" }, -- optional
-        requires = {
-            "nvim-treesitter/nvim-treesitter",
-            "nvim-telescope/telescope.nvim", -- optional
-        },
-    },
-    {
-        "jose-elias-alvarez/null-ls.nvim",
-        config = function()
-            require("anhtran.null-ls-config")
-        end,
-    },
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"elixir",
+					"heex",
+					"eex",
+					"html",
+					"lua",
+					"sql",
+					"html",
+					"python",
+					"markdown",
+					"yaml",
+				}, -- only install parsers for elixir and heex
+				-- ensure_installed = "all", -- install parsers for all supported languages
+				sync_install = false,
+				ignore_install = {},
+				highlight = {
+					enable = true,
+					disable = {},
+				},
+			})
+		end,
+	},
+	{
+		"lvimuser/lsp-inlayhints.nvim",
+		config = function()
+			require("lsp-inlayhints").setup()
+		end,
+		enabled = false,
+	},
+	{
+		"nvim-treesitter/nvim-treesitter",
+	},
+	{
+		"nvim-treesitter/nvim-treesitter-textobjects",
+		dependencies = "nvim-treesitter/nvim-treesitter",
+	},
+	{
+		"cuducos/yaml.nvim",
+		ft = { "yaml" }, -- optional
+		requires = {
+			"nvim-treesitter/nvim-treesitter",
+			"nvim-telescope/telescope.nvim", -- optional
+		},
+	},
+	{
+		"nvimtools/none-ls.nvim",
+		enabled = true,
+		dependencies = {
+			"nvimtools/none-ls-extras.nvim",
+		},
+		config = function()
+			require("anhtran.null-ls-config")
+		end,
+	},
 }
